b5c309422890409ce31d994de88076e6376d5563
Ryan Canton <ryancanton999@gmail.com>
2023-01-13 09:25:52 -0700

    Feat: Defined model relationships

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe PatientDoctor do
+RSpec.describe Patient do
   describe "relationships" do
     it {should have_many :patient_doctors}
     it {should have_many(:doctors).through(:patient_doctors)}
